da33de1ad8f17b8005d5ec9deea85c09e30e5c81
DeGriesche <mano.pavlakis@gmail.com>
2021-01-29 09:39:53 +0100

    view_item_wetter

--- www/ftui/js/pipe.js
+++ www/ftui/js/pipe.js
@@ -6,8 +6,8 @@ const lessEqual = (value) => input => input <= value;
 
 const less = (value) => input => input < value;
 
-const equals = (value) => input => input = value;
+const equals = (value) => input => input === value;
 
-const notEquals = (value) => input => input != value;
+const notEquals = (value) => input => input !== value;
 
 const prepend = (value) => prefix => prefix + "" + value;
